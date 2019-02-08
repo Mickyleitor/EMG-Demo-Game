bca43dc2a54b06b9b861b5d8e6f8aceff5ae0e24
Mickyleitor <micky@uma.es>
2019-02-08 23:01:28 +0100

    Added last Arduino code

--- Arduino/EMG_Game/EMG_Game.ino
+++ Arduino/EMG_Game/EMG_Game.ino
@@ -12,32 +12,16 @@
 #define WATCHDOG_PIN 2
 
 // Global constants and variables
-const unsigned int UMBRAL_MUESTRAS = 180;
-const unsigned int UMBRAL_SENAL = 12;
+const unsigned int UMBRAL_MUESTRAS = 90;
+const unsigned int UMBRAL_SENAL = 14;
 const unsigned UMBRAL_DERIVA = 50;
 const unsigned long PERIODO_SH = 200;
-volatile unsigned char CurrentCh;         //Current channel being sampled.
-volatile unsigned int ADC_Value = 0;    //ADC current value
-//volatile unsigned int buff[NUMCHANNELS][UMBRAL_MUESTRAS];
+volatile unsigned char CurrentCh;         //Canal del cual está leyendo el ADC
+volatile unsigned int ADC_Value = 0;    //Valor leído por el ADC
 unsigned long buff_1[NUMCHANNELS];
 unsigned long buff_envio[NUMCHANNELS];
 volatile unsigned int INDICE = 0;
 unsigned long UMBRAL_MUESTREO = 0;
-//
-
-void Toggle_LED1(void){
-
- if((digitalRead(LED1))==HIGH){ digitalWrite(LED1,LOW); }
- else{ digitalWrite(LED1,HIGH); }
- 
-}
-
-void Toggle_LED2(void){
-
- if((digitalRead(LED2))==HIGH){ digitalWrite(LED2,LOW); }
- else{ digitalWrite(LED2,HIGH); }
- 
-}
 
 //-------------------Estructuras-----------------
 struct Shield {
@@ -66,7 +50,7 @@ void loop_sh(struct Shield& sh,unsigned long curr_ms)
   if(INDICE >= UMBRAL_MUESTRAS){
     for(int i=0;i<NUMCHANNELS;i++){
       if(buff_1[i]>UMBRAL_SENAL){
-         Serial.print("SEÃ‘AL DETECTADA en el canal: ");
+         Serial.print("SEÑAL DETECTADA en el canal: ");
          Serial.println(i);
          buff_envio[i]=1;       
          buff_1[i]=0;
@@ -76,17 +60,22 @@ void loop_sh(struct Shield& sh,unsigned long curr_ms)
       }
     }
 
-    if((buff_envio[0] == 1)){
-      Serial1.write('D');
-      Serial.print("SEÃ‘AL ENVIADA en el canal: "); //ain 1 shield arriba
-      Serial.println(0);
+    if((buff_envio[0] == 1)&&(buff_envio[1]==1)){
+      Serial1.write('L');
+      Serial.print("SEÑAL ENVIADA en el canal: "); //ain 1 shield arriba
+      Serial.println("Disparo");
+    }else{
+      if((buff_envio[0] == 1)){
+        Serial1.write('D');
+        Serial.print("SEÑAL ENVIADA en el canal: ");
+        Serial.println(0);
       }
-    if((buff_envio[1] == 1)){
-      Toggle_LED1();
-      Serial1.write('I');
-      Serial.print("SEÃ‘AL ENVIADA en el canal: ");
-      Serial.println(1);
+      if((buff_envio[1] == 1)){
+        Serial1.write('I');
+        Serial.print("SEÑAL ENVIADA en el canal: ");
+        Serial.println(1);
       }
+    }
     INDICE = 0;
   }
   
@@ -99,12 +88,7 @@ struct Shield sh;
 
 void setup() {
 
-   noInterrupts();  // Disable all interrupts before initialization
-   // LED1
-   pinMode(LED1, OUTPUT);  //Setup LED1 direction
-   pinMode(6, OUTPUT);
-   digitalWrite(LED1,HIGH); //Setup LED1 state
-   digitalWrite(LED2,HIGH);
+   noInterrupts();
    FlexiTimer2::set(TIMER2VAL, Timer2_Overflow_ISR);
    FlexiTimer2::start();
    for(int i=0;i<NUMCHANNELS;i++){
@@ -120,11 +104,8 @@ void setup() {
    tone(WATCHDOG_PIN,1);
    attachInterrupt(digitalPinToInterrupt(WATCHDOG_PIN),watchdog,FALLING);
    
-   interrupts();  // Enable all interrupts after initialization has been completed
- 
+   interrupts();
 
-
-  
 }
 
 void loop() {
@@ -135,22 +116,14 @@ void loop() {
 //***** Rutina interrupcion Timer2 *****************
 void Timer2_Overflow_ISR()
 {
-  // Toggle LED1 with ADC sampling frequency /2
-  //Toggle_LED1();
-  
-  //Read the 6 ADC inputs and store current values in Packet
+  //Leemos cada canal de entrada
   for(CurrentCh=0;CurrentCh<NUMCHANNELS;CurrentCh++){
     ADC_Value = analogRead(CurrentCh);
     if(ADC_Value>UMBRAL_MUESTREO){
        buff_1[CurrentCh]++;
     }
-    //buff[CurrentCh][INDICE]=ADC_Value;
   }
   INDICE++;
-  /*
-  if(INDICE==UMBRAL_MUESTRAS){
-    INDICE=0;
-  }*/
 }
 void watchdog(){
   Serial1.write('d');
